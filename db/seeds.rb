c445a20de2f0d6f8444ea8bffe018691c49d8b5e
Noemi Heusler <nlheusler@gmail.com>
2020-02-26 11:39:02 +0100

    airbnb simple mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Paris',
+  address: '10 Clifton Gardens Paris W9 1DT',
+  description: 'Paris A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Nyon',
+  address: '10 Clifton Gardens Nyon W9 1DT',
+  description: 'Nyon A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 55,
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Bern',
+  address: '10 Clifton Gardens Bern W9 1DT',
+  description: 'Ben A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 95,
+  number_of_guests: 4
+)
